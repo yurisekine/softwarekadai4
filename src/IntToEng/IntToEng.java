a03bb616b3190ccda03ae24c855b855a5bd777b9
g14944sy <g14944sy@s20612.tsuda.ac.jp>
2016-05-18 18:24:08 +0900

    Kasumi3 commit

--- src/IntToEng/IntToEng.java
+++ src/IntToEng/IntToEng.java
@@ -1,38 +1,35 @@
 package IntToEng;
 import java.util.Scanner;
 
-
 public class IntToEng {
     // メインメソッド
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-
         System.out.println(translateEng(input));
     }
+    
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	String str = "";
-    	//String[] doubleArray = {"","teen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}; 
     	String[] doubleArray = {"","", "twen", "thir", "for", "fif", "six", "seven", "eigh", "nine"}; 
     	String[] singleArray = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	String teen = "teen";
-    	String ty = "ty";
     	String hun  = " hundred ";
     	int triplefigures = n / 100;
     	int doublefigures = n / 10; //0~9
     	int singlefigures = n % 10; //0~9
-    		
-    	if(triplefigures != 0) {
-    		//if(!= 0)
+    	
+    	if(n==1000) {
+    		str = "one thousand";
+    	}else if(triplefigures != 0) {
+    		if(n%100 != 0){
     		str = singleArray[triplefigures] + hun + translateEng(n % 100);
-    	} 
-    	
-    	
+    		}else{
+    			str = singleArray[triplefigures] + hun ;
+    		}
+    	}
     	if(doublefigures == 0) {//0-9
     		str = singleArray[singlefigures];
-    		//str = doubleArray(singleArray, singlefigures);
     		
     	}else if (doublefigures == 1) {  //10-19
     		//10,11,12,14
@@ -43,43 +40,20 @@ public class IntToEng {
     		}else if(singlefigures == 2){ //12
     			str = "twelve";
     		}else if(singlefigures == 4) {
-    			str = "four" + teen;
+    			str = "fourteen";
     		} else { //13,15,16,17,18,19
-    			str = doubleArray[singlefigures] + teen;    			
+    			str = doubleArray[singlefigures] + "teen";    			
     		}
     		
     	} else if(doublefigures >=2 && doublefigures <= 9){ //20-99
     		if(singlefigures == 0){ //20,30,40...    
-    			str = doubleArray[doublefigures] + ty;   			
-    		//	str = doubleArray(doubleArray, doublefigures);
+    			str = doubleArray[doublefigures] + "ty";   			
     		}else{
-    			str = doubleArray[doublefigures] + ty + " "+ singleArray[singlefigures];
+    			str = doubleArray[doublefigures] + "ty" + " "+ singleArray[singlefigures];
     		}
-    		
-    	} else if(doublefigures == 10){ //100
-    		if(singlefigures == 0){
-    			str = "one hundred"; 
-    		}
-        	
-        /*	if(triplefigures == 1) {
-        		str = singleArray[triplefigures] + hun + translateEng(n%100);//100
-        	} else if (triplefigures == 2) {
-        		
-        	}
-        	*/
     	}else{ //例外
     		//str = "0~100の数字を入力してください。";
     	}
-    	return str;
-    		
+    	return str;	
     }
-    
-    
-    
-/*	private static String doubleArray(String[] doubleArray, int doublefigures) {
-		String str;
-		str =doubleArray[doublefigures];
-		return str;
-	}*/
-
 }
